512e4aab3c5b41693efe83eec775d4ec47f7851b
Laurynas Lazauskas <8823448+lawrence-laz@users.noreply.github.com>
2023-10-07 20:17:28 +0300

    test: Replace test examples with the ones from zig language documentation

--- example/strings.zig
+++
@@ -1,7 +0,0 @@
-const std = @import("std");
-
-test "appending" {
-    const input = "Hello, ";
-    const expected = "Hello, world!";
-    try std.testing.expectEqualStrings(expected, input ++ "world!");
-}
